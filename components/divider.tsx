6e04ea00a5400afb0ddfa1b20b45716ebf6d0e8c
Leandro Dragani <leandro.dragani@extendeal.com>
2020-09-18 17:44:48 -0300

    wip

--- components/divider.tsx
+++ components/divider.tsx
@@ -10,6 +10,6 @@ export const Divider = styled.View<DividerProps>`
   align-self: stretch;
   border-bottom-width: ${StyleSheet.hairlineWidth}px;
   border-bottom-color: ${({ color, theme }) => color ?? theme.colors.gray[500]};
-  ${SS.space}
   opacity: 0.25;
+  ${SS.space}
 `;
